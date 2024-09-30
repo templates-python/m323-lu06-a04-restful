421f0694c381e891009a823643c9ca133c5cd1ca
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:07:43 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -8,25 +8,31 @@ app = ...
 dao = ...
 dao.create_table()
 
+
 def add_todo():
     # TODO: Implementiere das Hinzufügen eines neuen ToDo-Elements
     return jsonify({"message": "Not implemented yet"}), 501
+
 
 def get_all_todos():
     # TODO: Implementiere das Abrufen aller ToDo-Elemente
     return jsonify({"message": "Not implemented yet"}), 501
 
+
 def get_todo(item_id):
     # TODO: Implementiere das Abrufen eines einzelnen ToDo-Elements
     return jsonify({"message": "Not implemented yet"}), 501
+
 
 def update_todo(item_id):
     # TODO: Implementiere das Aktualisieren eines ToDo-Elements
     return jsonify({"message": "Not implemented yet"}), 501
 
+
 def delete_todo(item_id):
     # TODO: Implementiere das Löschen eines ToDo-Elements
     return jsonify({"message": "Not implemented yet"}), 501
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
